1301c09a387fc6649c4180113c05524da1975b74
Harison Mwangi <harrisonmwangi288@gmail.com>
2020-03-05 12:31:03 +0300

    Added functionality to display news feed

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from django.core import serializers
+from . models import feed
+import json
+
+# function-based views
+def index(request):
+    """ 
+    index() function is responsible to serve homepage of the website when user is online
+    results retrieves all the feeds and is sent to client side using context dict
+    """
+    template='posts/index.html'
+    results=feed.objects.all()
+    context={
+		'results':results,
+	}
+    return render(request,template,context)
+
+def base_layout(request):
+    """base_layout() function is used to render base.html"""
+    template='posts/base.html'
+    return render(request,template)
